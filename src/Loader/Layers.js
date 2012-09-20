2a63bafdd1d799da03ea9975aac63ce42c86b0b3
Micho Garcia <michogar@gmail.com>
2012-09-20 13:05:15 +0200

    Layers readed from file or external service

--- src/Loader/Layers.js
+++ src/Loader/Layers.js
@@ -15,63 +15,85 @@
 
 /**
  * Layers class uses Reader to get layers parameters from YAML file
- * 
+ *
  * TODO Tests
  */
 
 XVM.Loader.Layers = function(reader) {
-	
+
 	/**
 	 * Constant
 	 * {String}
 	 */
-	this.DEFAULTLAYERS = 'config/map.layers.yaml';	
-	
+	this.DEFAULTLAYERS = 'config/map.layers.yaml';
+
 	/**
 	 * Property
 	 * {XVM.Loader.Reader}
 	 */
 	this.reader = null;
-	
+
 	/**
 	 * Property
 	 * {Array(OpenLayer.Layers)}
 	 */
 	this.layers = new Array();
-	
+
 	/**
 	 * Method
 	 */
 	this._readLayers = function() {
 		this.reader.readFromFile(
 			this.DEFAULTLAYERS, 
-			function(response, context) {
-				var _this = context;
-				for (var n in response) {
-					var objectLayer = response[n];
-					var default_options = {
-						isBaseLayer : objectLayer.is_base,
-						visibility : objectLayer.visible,
-						singleTile : true,
-						opacity: 0.75,
-						transitionEffect : 'resize',
-						buffer : 0
-					};
-					var layer_options = $.extend(default_options, objectLayer.parameters);
-					var layer = new OpenLayers.Layer.WMS(
-						objectLayer.layer_name,
-						objectLayer.wms_url,
-						{
-							//GetMap parameters
-							transparent : true,
-							format : "image/png"
-						},
-						layer_options						
-					)
-					_this.layers.push(layer);
-				}
-				XVM.EventBus.fireaddLayers(_this.layers);
-			});			
+			this._readLayersCallback
+		);
+	}
+	
+	/**
+	 *
+	 */
+	this._readLayersCallback = function(response, context) {
+		var _this = context;
+		if(response.urlServiceLayer != null) {
+			_this.reader.readFromFile(
+				response.urlServiceLayer, 
+				_this._createLayers
+			);
+		} else {
+			var tmpresponse = response.layers;
+			_this._createLayers(tmpresponse, context);
+		}
+		XVM.EventBus.fireaddLayers(_this.layers);
+	}
+	
+	/**
+	 *
+	 */
+	this._createLayers = function(response, context) {
+		var _this = context;
+		for(var n in response) {
+			var objectLayer = response[n];
+			var default_options = {
+				isBaseLayer : objectLayer.is_base,
+				visibility : objectLayer.visible,
+				singleTile : true,
+				opacity : 0.75,
+				transitionEffect : 'resize',
+				buffer : 0
+			};
+			var layer_options = $.extend(default_options, objectLayer.parameters);
+			var layer = new OpenLayers.Layer.WMS(
+				objectLayer.layer_name, 
+				objectLayer.wms_url, 
+				{
+					//GetMap parameters
+					transparent : true,
+					format : "image/png"
+				}, 
+				layer_options
+			)
+			_this.layers.push(layer);
+		}
 	}
 	
 	/**
@@ -84,6 +106,6 @@ XVM.Loader.Layers = function(reader) {
 		this.reader.setContext(this);
 		this._readLayers();
 	}
-	
+
 	this.init(reader);
 }
--- test/spec/SpecLayers.js
+++ test/spec/SpecLayers.js
@@ -12,14 +12,50 @@
  */
 describe('Layers tests', function() {
 	
+	var response = {
+		urlServiceLayer : 'fakeLayersService',
+		layers:
+			[		
+			   {
+			      map_name:"default",
+			      layer_name:"IGN Base",
+			      wms_layer:"IGNBaseTodo",
+			      wms_url:"http:\/\/www.ign.es\/wms-inspire\/ign-base",
+			      visible:false,
+			      layer_position:512,
+			      layer_group:"Capas Base",
+			      parameters:{opacity: 0.75, singleTile: false},
+			      is_base:true
+			   },
+			   {
+			      map_name:"default",
+			      layer_name:"Topogr\u00e1fico-IDEE",
+			      wms_layer:"mtn_rasterizado",
+			      wms_url:"http:\/\/www.idee.es\/wms\/MTN-Raster\/MTN-Raster",
+			      visible:false,
+			      layer_position:510,
+			      layer_group:"Capas Base",
+			      parameters:{opacity: 0.60, singleTile: false},
+			      is_base:true
+			   }
+			]
+	};
+	
 	var reader = new XVM.Loader.Reader();
 	var layers;
-	
 	beforeEach(function() {
 		spyOn(reader, 'readFromFile');
+		layers = new XVM.Loader.Layers(reader);
 	});
-	
-	it('Inits control', function() {
-		layers = new XVM.Loader.Layers(reader);	
+
+	it('Response has urlServiceLayer reads url', function() {
+		layers._readLayersCallback(response, layers);
+		expect(reader.readFromFile.mostRecentCall.args[0]).toEqual('fakeLayersService');
+	})
+		
+	it('Get layer from json response', function() {
+		layers._createLayers(response.layers, layers);
+		expect(layers.layers.length).toEqual(response.layers.length);
+		expect(layers.layers[0].opacity).toEqual(0.75);
 	});
 });
