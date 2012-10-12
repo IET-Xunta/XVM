c109fc781043c4d12484f4c7dfd82f4dd4a032e2
Micho Garcia <michogar@gmail.com>
2012-10-12 22:25:15 +0200

    Added MousePosition control
    Added with necessary folder structure:
    	- Control name Folder
    	  - ControlName.js (file with code)
    	  - ControlName.yaml (file with config parameters)

---
+++ src/Control/MousePosition/MousePosition.js
@@ -0,0 +1,16 @@
+/**
+ * XVM: Xeovisor Minimo
+ * ----------------------------------------------
+ * Copyright (c) 2012, Xunta de Galicia. All rights reserved.
+ * Code licensed under the BSD License:
+ *   LICENSE.txt file available at the root application directory
+ *
+ * @author Instituto Estudos do Territorio, IET
+ */
+
+XVM.Control.MousePosition = XVM.Class.extend({
+	
+	initialize : function() {
+		
+	}
+})
