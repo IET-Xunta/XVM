ae07c79532f8ffac56bf3d785cc4fa1c7c920e73
Jorge Lopez Fernandez <lopez.fernandez.jorge@gmail.com>
2012-10-31 01:44:21 +0100

    First version of Navigation and Zoom controls.
    There is still some work to do on them, but they
    seem to work.

---
+++ src/Control/ZoomBox/ZoomBox.js
@@ -0,0 +1,32 @@
+/**
+ * XVM: Xeovisor Minimo
+ * ----------------------------------------------
+ * Copyright (c) 2012, Xunta de Galicia. All rights reserved.
+ * Code licensed under the BSD License:
+ *   LICENSE.txt file available at the root application directory
+ *
+ * @author Instituto Estudos do Territorio, IET
+ */
+
+XVM.Control.ZoomBox = XVM.Control.extend({
+	
+	OLMap : null,
+	
+	options : {},
+	
+	OLControl : null,
+	
+	initialize : function(params) {
+		this.options = params;
+	},
+	
+	setOLMap : function(map) {
+		this.OLMap = map;
+		this.createControl();
+	},
+	
+	createControl : function() {
+		this.OLControl = new OpenLayers.Control.ZoomBox(this.options);
+		this.OLMap.panel.addControls([this.OLControl]);
+	}
+});
