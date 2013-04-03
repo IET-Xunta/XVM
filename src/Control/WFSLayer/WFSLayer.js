0735bccf8faf83669f9bd18fc60d3a7ac9148c35
Micho Garcia <michogar@gmail.com>
2013-04-03 17:47:48 +0200

    WFS Layer
    WFS layer control searchs and selects feature

--- src/Control/WFSLayer/WFSLayer.js
+++ src/Control/WFSLayer/WFSLayer.js
@@ -20,51 +20,91 @@ XVM.Control.WFSLayer = XVM.Control.extend({
 	/**
 	 * Save WFS layers
 	 * Property: wfsLayer
-	 * [OpenLayers.Protocol.WFS]
+	 * OpenLayers.Layer.Vector
 	 */	
-	protocol : null,
+	vectorLayers : [],
 
 	createControl : function() {
 		this.OLControl = new OpenLayers.Control();
+		XVM.EventBus.addListener(this, 'searchAndZoom', 'searchAndZoom');
 	},
 	
-	saveProtocols : function() {
+	createVectorLayer : function() {
 
-		var protocolparams = this.options.protocol;
+		var protocols = this.options.protocols;
 		
-		var protocol = new OpenLayers.Protocol.WFS({
-               url:  protocolparams.url,
-               featureType: protocolparams.featureType,
-               featurePrefix: protocolparams.featurePrefix,
-               featureNS: protocolparams.featureNS,
-               srsName: protocolparams.srsName,
-               maxFeatures: protocolparams.maxFeatures
+		for (var i=0; i<protocols.length; i++) {
+			var protocol = new OpenLayers.Protocol.WFS({
+	               url:  protocols[i].url,
+	               featureType: protocols[i].featureType,
+	               featurePrefix: protocols[i].featurePrefix,
+	               featureNS: protocols[i].featureNS,
+	               srsName: protocols[i].srsName,
+	               maxFeatures: protocols[i].maxFeatures
+			});
+			protocol.name = protocols[i].layer_name;
+
+			var vectorLayer = new OpenLayers.Layer.Vector(protocol.name, 
+					{
+						strategies : [new OpenLayers.Strategy.BBOX()],
+						protocol : protocol,
+						styleMap: new OpenLayers.StyleMap(protocols[i].styleMap),
+						searchFields : protocols[i].searchFields,
+						visibility : protocols[i].visibility
+					});
+
+			this.vectorLayers.push(vectorLayer);
+		}
+		
+		XVM.EventBus.fireEvent('addLayers', this.vectorLayers);
+	},
+	
+	searchAndZoom : function(params) {
+
+		var filters = [];
+		for (var field in params.queryParams) {
+			var filter = new OpenLayers.Filter.Comparison({
+	                type: OpenLayers.Filter.Comparison.EQUAL_TO,
+	                property: field,
+	                value: params.queryParams[field]
+            });
+			filters.push(filter);
+		}
+		var layer = params.layer;
+		var protocol = layer.protocol;
+		
+		var OLfilter = new OpenLayers.Filter.Logical({
+         	type: OpenLayers.Filter.Logical.OR,
+         	filters: filters
 		});
-		
-		protocol.name = protocolparams.layer_name;
-		
-		console.log(protocol);
-
-		var vectorLayer = new OpenLayers.Layer.Vector(protocol.name, 
-				{
-					strategies : [new OpenLayers.Strategy.BBOX()],
-					protocol : protocol,
-					styleMap: new OpenLayers.StyleMap(this.options.styleMap)
-				});
 		
 		protocol.read({
-			callback: function(evt) {
-				console.log(evt);
+			filter : OLfilter,
+			callback : function(evt) {
+				if (evt.priv.status != 200) {
+					alert('Error en respuesta'); // TODO localice
+					return;
+				}
+				var features = evt.features;
+				var searchBounds = new OpenLayers.Bounds();
+				
+				for (var j=0; j<features.length; j++) {
+						var feature = layer.getFeatureByFid(features[j].fid);
+						searchBounds.extend(feature.geometry.bounds);
+				};
+
+				XVM.EventBus.fireEvent('setExtent', 
+						{'extent' : searchBounds, 
+						'closest' : false}
+				);
 			}
 		});
-			
-		XVM.EventBus.fireEvent('addLayers', [vectorLayer]);
 	},
 
 	beforeAddingControl : function() {
 	},
 	
 	afterAddingControl : function() {
-		this.saveProtocols();
+		this.createVectorLayer();
 	}
 });
