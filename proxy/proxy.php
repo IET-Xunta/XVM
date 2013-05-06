81024b1faa50d065ac495ab7e4fb88babf1998ab
Nacho Varela <nachouve@gmail.com>
2013-05-06 08:48:14 +0200

    Improving proxy.php

--- proxy/proxy.php
+++ proxy/proxy.php
@@ -93,7 +93,43 @@ if (!in_array($p_url['host'], $allowed_urls)){
    return;
 };
 
-$ch = curl_init();
+// GET, POST, PUT, HEAD, DELETE, ect ...
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+//$ch = curl_init();
+// init curl session
+$ch = $session = curl_init(substr($_SERVER['QUERY_STRING'], 4));
+
+
+// curl headers array
+$headers= array();
+foreach(getallheaders() as $key => $value)
+    $headers[] = $key.': '.$value;
+
+// curl options
+$opts   = array(
+    CURLOPT_HEADER => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_BINARYTRANSFER => true,
+    CURLOPT_CUSTOMREQUEST => $method,
+    CURLOPT_HTTPHEADER => $headers
+);
+
+// if request is post ...
+if($method === 'POST'){
+    // populate the array of keys/values to send
+    $headers = array();
+    foreach($_POST as $key => $value){
+        $headers[] = rawurlencode($key).'='.rawurlencode($value);
+	}
+    $opts[CURLOPT_POST] = 1;
+    $opts[CURLOPT_POSTFIELDS] = implode('&', $headers);
+	curl_setopt_array($ch, $opts);
+}
+
+
+
 curl_setopt($ch, CURLOPT_URL,$url);
 //curl_setopt($ch, CURLOPT_PROXY, $proxy);
 //curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
@@ -103,17 +139,42 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 //When using WFS request it is needed
 $request_body = file_get_contents('php://input');
 if ($request_body){
+	if ($DEBUG==2) {
+		$logger->msg("====== HEADER request"."\n");
+		$logger->msg($_SERVER);	
+		$logger->msg("====== REQUEST_BODY"."\n");
+		$logger->msg($request_body);
+	}
+	//header("Content-type: ".$_SERVER['CONTENT_TYPE']);
+	//header("Content-length: ".$_SERVER['CONTENT_LENGTH']);
+	curl_setopt($ch, CURLINFO_HEADER_OUT, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		"Content-type: ".$_SERVER['CONTENT_TYPE'],
+		"Content-length: ".$_SERVER['CONTENT_LENGTH'],
+	));
 }
 
 //curl_setopt($ch, CURLOPT_HEADER, 1);
 $curl_scraped_page = curl_exec($ch);
+
+$content_info = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+ 
 curl_close($ch);
 
 if ($DEBUG==2) {
+	$headers = curl_getinfo($ch, CURLINFO_HEADER_OUT);
+	$logger->msg("====== HEADER"."\n");
+	$logger->msg($header);
+	$logger->msg("====== CONTENT_INFO"."\n");
+	$logger->msg($content_info);
+	$logger->msg("====== ANSWER"."\n");
 	$logger->msg($curl_scraped_page);
 }
 
+
+header("Content-type: ".$content_info);
 echo $curl_scraped_page;
+
 
 ?>
